fa789fba51e861abc358cb9bc8fc10509e73cd4b
jennifermorgan2016 <jennifer.morgan@ise.ie>
2018-10-03 14:58:34 +0100

    Say Hi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 // Create a new component.
 // This component should produce some HTML
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = function() {
+  return <div>Hi!</div>;
+}
 
 // Take this component's generated HTML and put it 
 // on the page (in the DOM)
+
+
+ReactDOM.render(<App />, document.getElementsByClassName('container')[0]);
 
